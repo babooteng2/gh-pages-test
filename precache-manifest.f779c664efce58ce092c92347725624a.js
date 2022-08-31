d2602dcfb68e6c7783ab0fff6936385779a4946d
kiasarm <kiasarm@automarketing.co.kr>
2022-08-31 16:52:38 +0900

    Updates

---
+++ precache-manifest.f779c664efce58ce092c92347725624a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "865976bf1a807b968668",
+    "url": "/gh-pages-test/static/js/main.865976bf.chunk.js"
+  },
+  {
+    "revision": "4395dc68df27edc93821",
+    "url": "/gh-pages-test/static/js/1.4395dc68.chunk.js"
+  },
+  {
+    "revision": "3f800456919005af430e",
+    "url": "/gh-pages-test/static/js/runtime~main.3f800456.js"
+  },
+  {
+    "revision": "cc55971d2fb43ce49ea4c7acdf68c2a7",
+    "url": "/gh-pages-test/index.html"
+  }
+];
